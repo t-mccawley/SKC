a27a6a1ea7fa5b2a9080b1ff6ddc5d8b71076082
Travis McCawley <targetdog88@gmail.com>
2020-06-15 20:27:46 -0700

    Added scroll bar!

--- config.lua
+++ config.lua
@@ -33,7 +33,7 @@ function Config:GetThemeColor()
 end
 
 function Config:CreateButton(point, relativeFrame, relativePoint, yOffset, text)
-	local btn = CreateFrame("Button", nil, UIConfig, "GameMenuButtonTemplate");
+	local btn = CreateFrame("Button", nil, UIConfig.ScrollFrame, "GameMenuButtonTemplate");
 	btn:SetPoint(point, relativeFrame, relativePoint, 0, yOffset);
 	btn:SetSize(140, 40);
 	btn:SetText(text);
@@ -42,20 +42,47 @@ function Config:CreateButton(point, relativeFrame, relativePoint, yOffset, text)
 	return btn;
 end
 
+local function ScrollFrame_OnMouseWheel(self,delta)
+    -- delta: 1 scroll up, -1 scroll down
+    -- value at top is 0, value at bottom is size of child
+    local scroll_range = self:GetVerticalScrollRange()
+    local newValue = math.min( scroll_range , math.max( 0 , self:GetVerticalScroll() - (0.1*scroll_range*delta) ) );
+    self:SetVerticalScroll(newValue);
+    return
+end
+
 function Config:CreateMenu()
-	UIConfig = CreateFrame("Frame", "SKC_Config", UIParent, "BasicFrameTemplateWithInset");
-	UIConfig:SetSize(200, 360);
+	UIConfig = CreateFrame("Frame", "SKC_Config", UIParent, "UIPanelDialogTemplate");
+	UIConfig:SetSize(350, 400);
 	UIConfig:SetPoint("CENTER"); -- Doesn't need to be ("CENTER", UIParent, "CENTER")
 
-	UIConfig.title = UIConfig:CreateFontString(nil, "OVERLAY", "GameFontHighlight");
-	UIConfig.title:SetPoint("LEFT", UIConfig.TitleBg, "LEFT", 5, 0);
-	UIConfig.title:SetText("SKC Configuration");
+    UIConfig.Title:ClearAllPoints();
+    UIConfig.Title:SetFontObject("GameFontHighlight");
+	UIConfig.Title:SetPoint("LEFT", SKC_ConfigTitleBG, "LEFT", 6, 0);
+    UIConfig.Title:SetText("SKC Configuration");
+    
+    UIConfig.ScrollFrame = CreateFrame("ScrollFrame",nil,UIConfig,"UIPanelScrollFrameTemplate");
+    UIConfig.ScrollFrame:SetPoint("TOPLEFT",SKC_ConfigDialogBG,"TOPLEFT",4,-8);
+    UIConfig.ScrollFrame:SetPoint("BOTTOMRIGHT",SKC_ConfigDialogBG,"BOTTOMRIGHT",-3,4);
+
+    UIConfig.ScrollFrame:SetClipsChildren(true);
+
+    local child = CreateFrame("Frame",nil,UIConfig.ScrollFrame);
+    child:SetSize(308,500);
+
+    UIConfig.ScrollFrame:SetScrollChild(child);
+
+    UIConfig.ScrollFrame:SetScript("OnMouseWheel",ScrollFrame_OnMouseWheel)
+
+    UIConfig.ScrollFrame.ScrollBar:ClearAllPoints();
+    UIConfig.ScrollFrame.ScrollBar:SetPoint("TOPLEFT",UIConfig.ScrollFrame,"TOPRIGHT",-12,-18);
+    UIConfig.ScrollFrame.ScrollBar:SetPoint("BOTTOMRIGHT",UIConfig.ScrollFrame,"BOTTOMRIGHT",-7,18);
 
 	----------------------------------
 	-- Buttons
 	----------------------------------
 	-- Save Button:
-	UIConfig.saveBtn = self:CreateButton("CENTER", UIConfig, "TOP", -70, "Save");
+	UIConfig.saveBtn = self:CreateButton("CENTER", child, "TOP", -70, "Save");
 
 	-- Reset Button:	
 	UIConfig.resetBtn = self:CreateButton("TOP", UIConfig.saveBtn, "BOTTOM", -10, "Reset");
@@ -67,7 +94,7 @@ function Config:CreateMenu()
 	-- Sliders
 	----------------------------------
 	-- Slider 1:
-	UIConfig.slider1 = CreateFrame("SLIDER", nil, UIConfig, "OptionsSliderTemplate");
+	UIConfig.slider1 = CreateFrame("SLIDER", nil, UIConfig.ScrollFrame, "OptionsSliderTemplate");
 	UIConfig.slider1:SetPoint("TOP", UIConfig.loadBtn, "BOTTOM", 0, -20);
 	UIConfig.slider1:SetMinMaxValues(1, 100);
 	UIConfig.slider1:SetValue(50);
@@ -75,7 +102,7 @@ function Config:CreateMenu()
 	UIConfig.slider1:SetObeyStepOnDrag(true);
 
 	-- Slider 2:
-	UIConfig.slider2 = CreateFrame("SLIDER", nil, UIConfig, "OptionsSliderTemplate");
+	UIConfig.slider2 = CreateFrame("SLIDER", nil, UIConfig.ScrollFrame, "OptionsSliderTemplate");
 	UIConfig.slider2:SetPoint("TOP", UIConfig.slider1, "BOTTOM", 0, -20);
 	UIConfig.slider2:SetMinMaxValues(1, 100);
 	UIConfig.slider2:SetValue(40);
@@ -86,12 +113,12 @@ function Config:CreateMenu()
 	-- Check Buttons
 	----------------------------------
 	-- Check Button 1:
-	UIConfig.checkBtn1 = CreateFrame("CheckButton", nil, UIConfig, "UICheckButtonTemplate");
+	UIConfig.checkBtn1 = CreateFrame("CheckButton", nil, UIConfig.ScrollFrame, "UICheckButtonTemplate");
 	UIConfig.checkBtn1:SetPoint("TOPLEFT", UIConfig.slider1, "BOTTOMLEFT", -10, -40);
 	UIConfig.checkBtn1.text:SetText("My Check Button!");
 
 	-- Check Button 2:
-	UIConfig.checkBtn2 = CreateFrame("CheckButton", nil, UIConfig, "UICheckButtonTemplate");
+	UIConfig.checkBtn2 = CreateFrame("CheckButton", nil, UIConfig.ScrollFrame, "UICheckButtonTemplate");
 	UIConfig.checkBtn2:SetPoint("TOPLEFT", UIConfig.checkBtn1, "BOTTOMLEFT", 0, -10);
 	UIConfig.checkBtn2.text:SetText("Another Check Button!");
 	UIConfig.checkBtn2:SetChecked(true);
